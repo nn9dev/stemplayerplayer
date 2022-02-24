6f60818f712040627b10f39ec8e2b9f5ec8dff68
nn9dev <39563889+nn9dev@users.noreply.github.com>
2022-02-24 00:04:08 -0500

    Add files via upload

---
+++ stemplayerplayer.py
@@ -0,0 +1,72 @@
+import keyboard
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame as pg
+import time
+
+pg.mixer.init()
+#pg.init()
+
+a1Note = pg.mixer.Sound("C:\stemplayer\\nmwi\\nmwi1.mp3")
+a2Note = pg.mixer.Sound("C:\stemplayer\\nmwi\\nmwi2.mp3")
+a3Note = pg.mixer.Sound("C:\stemplayer\\nmwi\\nmwi3.mp3")
+a4Note = pg.mixer.Sound("C:\stemplayer\\nmwi\\nmwi4.mp3")
+
+#pg.mixer.set_num_channels(50)
+
+a1Note.play()
+a2Note.play()
+a3Note.play()
+a4Note.play()
+
+print (pg.mixer.get_busy())
+
+while pg.mixer.get_busy() == True:
+    #print(a1Note.get_volume())
+    #print(a2Note.get_volume())
+    #print(a3Note.get_volume())
+    #print(a4Note.get_volume())
+    
+    if keyboard.is_pressed('1'):
+        print ("1")
+        if a1Note.get_volume() == 1.0:
+            a1Note.set_volume(0.0)
+        elif a1Note.get_volume() == 0.0:
+            a1Note.set_volume(1.0)
+        print(a1Note.get_volume())
+        while keyboard.is_pressed('1'):
+            keyboard.block_key('1')
+        keyboard.unhook_all()
+            
+    if keyboard.is_pressed('2'):
+        print ("2")
+        if a2Note.get_volume() == 1.0:
+            a2Note.set_volume(0.0)
+        elif a2Note.get_volume() == 0.0:
+            a2Note.set_volume(1.0)
+        print(a2Note.get_volume())
+        while keyboard.is_pressed('2'):
+            keyboard.block_key('2')
+        keyboard.unhook_all()
+            
+    if keyboard.is_pressed('3'):
+        print("3")
+        if a3Note.get_volume() == 1.0:
+            a3Note.set_volume(0.0)
+        elif a3Note.get_volume() == 0.0:
+            a3Note.set_volume(1.0)
+        print(a3Note.get_volume())
+        while keyboard.is_pressed('3'):
+            keyboard.block_key('3')
+        keyboard.unhook_all()
+            
+    if keyboard.is_pressed('4'):
+        print("4")
+        if a4Note.get_volume() == 1.0:
+            a4Note.set_volume(0.0)
+        elif a4Note.get_volume() == 0.0:
+            a4Note.set_volume(1.0)
+        print(a4Note.get_volume())
+        while keyboard.is_pressed('4'):
+            keyboard.block_key('4')
+        keyboard.unhook_all()
